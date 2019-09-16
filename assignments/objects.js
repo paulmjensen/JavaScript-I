3d55dba65517400437d2fd52fdf6ab3a564aca41
Paul Jensen <PJensen@pauls-mbp.home>
2019-09-16 15:40:43 -0400

    Initial Commit (finished object JS assignment

--- assignments/objects.js
+++ assignments/objects.js
@@ -17,28 +17,68 @@ const example = {
   gender: "F",
 }
 
-// Write your intern objects here:
 
+// Write your intern objects here:
+const MitziObject = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmeloy0@psu.edu",
+  gender: "F",
+}
+
+const KennanObject = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: function(){
+    return "Hello, my name is Kennan!"}
+}
+
+const KevenObject = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M",
+}
+
+const GannieObject = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "F",
+}
+
+const AntoniettaObject = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+  multiplyNums: function(num1, num2){
+    return num1 * num2;
+  }
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(MitziObject.name);
 // Kennan's ID
-
+console.log(KennanObject.id);
 // Keven's email
-
+console.log(KevenObject.email);
 // Gannie's name
-
+console.log(GannieObject.name);
 // Antonietta's Gender
-
+console.log(AntoniettaObject.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+console.log(KennanObject.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(AntoniettaObject.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
